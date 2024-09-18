190353df75fed9803d1b6f03be65d5617e175cfc
taylorcs827 <taylorcs827@gmail.com>
2024-09-17 19:00:32 -0500

    Twimba

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: '@Leggo2 💎',
+        profilePic: 'images/bolt.png',
+        likes: 1075,
+        retweets: 110,
+        tweetText: 'Buy Bitcoin!!',
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: '@Lane ✅',
+        profilePic: 'images/home.png',
+        likes: 500,
+        retweets: 24,
+        tweetText: 'Going to the moon!.',
+        replies: [
+                  {
+                handle: '@Biggie ✅',
+                profilePic: 'images/person.png',
+                tweetText: 'Sign me up! 😎🛩',
+            },
+                  {
+                handle: '@Chuck ✅',
+                profilePic: 'images/face.png',
+                tweetText: 'I went last week',
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: '@CoderEnvy',
+        profilePic: 'images/emoji_people.png',
+        likes: 104,
+        retweets: 63,
+        tweetText: 'Let me hear from ya!',
+        replies: [
+            {
+                handle: '@StackOverflower ☣️',
+                profilePic: 'images/rocket_launch.png',
+                tweetText: 'No.',
+            },
+            {
+                handle: '@YummyCoder64',
+                profilePic: 'images/man.png',
+                tweetText: 'Ugh. Not again ❤️',
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
